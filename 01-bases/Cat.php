0b8aa64eb9fb4e606c38e831d70c25bb2c9af733
Robin Payet <robinpayet@gmail.com>
2023-03-01 12:12:36 +0100

    Constructor et Function

--- 01-bases/Cat.php
+++ 01-bases/Cat.php
@@ -1,10 +1,25 @@
 <?php
 
-class Cat {
+class Cat 
+{
 
     public $name;
-    public $type;
+    public $type = 'Européen'; //Same
     public $fur;
+
+    public function __construct($name, $type = 'Européen', ) // type Same
+    {
+        // Le constructeur est appelé quand on crée un chat.
+        // $this représente l'objet qui vient d'être créé.
+        // Ici, on initialise les données de l'objet
+        $this->name = $name;
+        $this->type = $type;
+    }
+
+    public function cry() 
+    {
+        return 'Miaou !';
+    }
 
 }
 
--- 01-bases/index.php
+++ 01-bases/index.php
@@ -4,11 +4,16 @@ require __DIR__.'/Cat.php';
 
 
 // Un objet (instance de classe)
-$bianca = new Cat();
-$bianca->name = 'Bianca';
+$bianca = new Cat('Bianca');
+// $bianca->name = 'Bianca';
+$bianca->fur = 'Blanc';
 
-$mina = new Cat();
+$mina = new Cat('Mina', 'Siamois');
+$mina->fur = 'Noir';
 
 var_dump($bianca);
 var_dump($mina);
 ?>
+
+<h1>Mon premier chat s'appelle <?= $bianca->name; ?></h1>
+<p>Il peut faire <?= $bianca->cry() ?></p>
