3c717d59231ea86e3e1993c5e2c1cdba46ef98d8
Rpayet <robinpayet@gmail.com>
2023-03-07 08:59:22 +0100

    Tuesday Commit Starter

--- 04-mvc/src/Model/Model.php
+++ 04-mvc/src/Model/Model.php
@@ -8,11 +8,22 @@ abstract class Model
 {
     public static function all()
     {
+        $table = self::table();
+
+        return DB::select("SELECT * FROM $table");
+    }
+
+
+    /**
+     * Permet de générer le nom de la table par rapport au nom de la table
+     */
+    private static function table(): string
+    {
         $table = get_called_class(); //M2i\Mvc\Model\Movie
         $table = substr(strrchr($table, '\\'), 1);
         $table = strtolower($table);
-        
-        return DB::select("SELECT * FROM $table");
+
+        return$table;
     }
 }
 ?>
